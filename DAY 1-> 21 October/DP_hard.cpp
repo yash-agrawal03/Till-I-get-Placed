50d34c39ec23b7ab28895229376acacce97ed31d
yash-agrawal03 <yash.agrawal1589@gmail.com>
2023-10-21 23:39:59 +0530

    did 1 question for the day 1

---
+++ DAY 1-> 21 October/DP_hard.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+class Solution
+{
+public:
+    int constrainedSubsetSum(vector<int> &nums, int k)
+    {
+        int n = nums.size();
+        vector<int> dp(n);
+
+        for (int i = 0; i < n; i++)
+        {
+            dp[i] = nums[i];
+        }
+        priority_queue<pair<int, int>> pq;
+        pq.push({dp[0], 0});
+        int result = dp[0];
+
+        for (int i = 1; i < n; i++)
+        {
+            while (!pq.empty() and i - pq.top.second > k)
+            {
+                pq.pop();
+            }
+            dp[i] = max(dp[i], nums[i] + pq.top().first);
+            pq.push({dp[i], i});
+            result = max(result, dp[i]);
+        }
+
+        return result;
+    }
+};
